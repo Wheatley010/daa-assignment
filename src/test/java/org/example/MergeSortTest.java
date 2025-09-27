d7964525320ae09cc5c31b57ec9e52c24f50fdcc
Timur Imbergenov <timurimbergenov@gmail.com>
2025-09-27 23:39:26 +0500

    chore: finalize all algorithms and tests

--- src/test/java/org/example/MergeSortTest.java
+++ src/test/java/org/example/MergeSortTest.java
@@ -5,17 +5,18 @@ import java.util.Random;
 import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-class MergeSortTest{
+class MergeSortTest {
     @Test
-    void randomTests(){
-        Random rnd=new Random(123);
-        int[] sizes={0,1,2,10,50,100,1000};
-        for(int n:sizes){
-            int[] a=rnd.ints(n,-1000,1000).toArray();
-            int[] b=Arrays.copyOf(a,a.length);
-            MergeSort.sort(a);
+    void randomTests() {
+        Random rnd = new Random(123);
+        int[] sizes = {0, 1, 2, 10, 50, 100, 1000};
+        for(int n : sizes) {
+            int[] a = rnd.ints(n, -1000, 1000).toArray();
+            int[] b = Arrays.copyOf(a, a.length);
+            Metrics metrics = new Metrics(); // создаём Metrics
+            MergeSort.sort(a, metrics);       // передаём его в сортировку
             Arrays.sort(b);
-            assertArrayEquals(b,a);
+            assertArrayEquals(b, a);
         }
     }
 }
